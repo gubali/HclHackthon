9b7cd0062b97b1b762efab4bc2eacea3fece7616
gubali <masif_apps@live.com>
2025-07-05 17:03:12 +0530

    updated be code

--- src/app/component/patientmangement/patient/patient.ts
+++ src/app/component/patientmangement/patient/patient.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Patient } from '../../../models/patient/patient.model.';
+import { PatientService } from '../../../services/patient-service';
 @Component({
   selector: 'app-patient',
   imports: [],
@@ -8,6 +9,9 @@ import { Patient } from '../../../models/patient/patient.model.';
 })
 export class PatientComponent implements OnInit {
   patient!: Patient;
+  patients: Patient[] = [];
+  errorMessage: string = '';
+  constructor(private patientService: PatientService) {}
   ngOnInit(): void {
     this.patient = new Patient({
       fullName: 'Jane Doe',
@@ -22,5 +26,21 @@ export class PatientComponent implements OnInit {
       emergencyContactPhone: '+0987654321',
       profileImage: 'https://randomuser.me/api/portraits/women/44.jpg',
     });
+    this.getPatients();
+  }
+
+  getPatients(): void {
+    this.patientService.getPatients().subscribe({
+      next: (data) => {
+        this.patients = Array.isArray(data) ? data : [data];
+        console.log('Patients data loaded:', this.patients);
+        this.errorMessage = '';
+      },
+      error: (error) => {
+        this.errorMessage = 'Failed to load patients data';
+        console.error('Error fetching patients:', error);
+      },
+    });
   }
 }
+export { Patient };
---
+++ src/app/services/patient-service.ts
@@ -0,0 +1,15 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Patient } from '../models/patient/patient.model.';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PatientService {
+ private apiUrl = 'http://localhost:5000/api/patients';
+  constructor(private http: HttpClient) { }
+   getPatients(): Observable<Patient> {
+    return this.http.get<any>(this.apiUrl);
+  }
+}
